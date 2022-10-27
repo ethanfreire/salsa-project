6f50bf77c42c86f475cc5cd587e3227361052855
ethanfreire <element55star@gmail.com>
2022-10-27 18:03:24 -0400

    finish implementation get/planets api

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,5 +1,7 @@
-const parse = require("csv-parse");
+const {parse} = require('csv-parse');
 const fs = require("fs");
+const path = require('path');
+
 
 const habitablePlanets = [];
 
@@ -10,39 +12,37 @@ function isHabitablePlanet(planet) {
     planet["koi_insol"] < 1.11 &&
     planet["koi_prad"] < 1.6
   );
-};
+}
 
 
-
-
-
-
-fs.createReadStream("../data/kepler_data.csv")
-  .pipe(
-    parse({
-      comment: "#",
-      columns: true,
-    })
-  )
-  .on("data", () => {
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
-    }
-  })
-  .on("error", (err) => {
-    console.log(err);
-  })
-  .on("end", () => {
-    console.log(
-      habitablePlanets.map((planet) => {
-        return planet["kepler_name"];
+ function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream( path.join(__dirname, '..', '..', 'data', 'kepler_data.csv' ))
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
       })
-    );
-    console.log(
-      `${habitablePlanets.length} habitable planets have been found woot.`
-    );
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", () => {
+        console.log(
+          `${habitablePlanets.length} habitable planets have been found woot.`
+        );
+        resolve();
+      });
   });
+}
 
 module.exports = {
+  loadPlanetsData,
   planets: habitablePlanets,
 };
--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,16 @@
 const PORT = process.env.PORT || 8000;
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
+const { loadPlanetsData } = require("./models/planets.model");
+
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
     console.log(`Listening in on PORT ${PORT} .`);
-});
+  });
+}
+
+startServer();
